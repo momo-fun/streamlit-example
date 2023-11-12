b628d79afee5f9d39dc9ed56d224980cff4e3f4b
momo-fun <149035214+momo-fun@users.noreply.github.com>
2023-11-12 11:20:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,40 +3,28 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import plotly.graph_objs as go
+import streamlit as st
 
-"""
-# Welcome to Streamlit!happy coding!happy coding!happy coding!
+# Create some example data
+x_data = ['A', 'B', 'C', 'D', 'E']
+y1_data = [1, 2, 3, 2, 1]
+y2_data = [2, 1, 2, 3, 4]
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Create a bar trace
+bar_trace = go.Bar(x=x_data, y=y1_data, name='Bar data')
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Create a line trace
+line_trace = go.Scatter(x=x_data, y=y2_data, name='Line data', yaxis='y2')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
+# Create a layout with a secondary y-axis
+layout = go.Layout(
+    yaxis=dict(title='Bar data'),
+    yaxis2=dict(title='Line data', overlaying='y', side='right'),
+)
 
-happy coding!
-"""
+# Create a figure and add the traces
+fig = go.Figure(data=[bar_trace, line_trace], layout=layout)
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-    print('happy?')
-    
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
-    
+# Display the figure in Streamlit
+st.plotly_chart(fig)
